b056d623163f246594c84d98c2a135d236b17695
baranowb <baranowb@gmail.com>
2019-02-25 10:47:26 +0100

    Add rever patch

--- run-test.sh
+++ run-test.sh
@@ -6,7 +6,7 @@ readonly TEST=${TEST}}
 
 set -u
 
-patch -p1 ${REPRODUCER_PATCH}
+patch -p1 -i ${REPRODUCER_PATCH}
 # TODO if patch fails, we need to skip test and print a message that the test is not compatible with the revision skipped
 
 if [ -z "${TEST}" ]; then
@@ -18,16 +18,12 @@ echo -n 'Building ...'
 
 export TESTSUITE_OPTS="-DskipTests"
 /opt/jboss-set-ci-scripts/harmonia-eap-build
-# because of -e, the following block is no longer required:
-#if [ $? -ne 0 ]; then
-#		echo "GIT-BISECT: Failed to build project."
-#		#NOTE: this can happen if commits are half assed.
-#		return 1
-#	fi
+
 echo 'Done.'
 
 echo -n 'Running testsuite ...'
 export TESTSUITE_OPTS="-Dtest=$TEST"
 /opt/jboss-set-ci-scripts/harmonia-eap-build 'testsuite'
 
-# TODO: Reverse the patch to ensure git is not perturb by new files or modify file in the repository
+echo -n 'Cleaning up after patch ...'
+patch -p1 -i ${REPRODUCER_PATCH} -R
