9ca5e6451fdfc8303d5f595a0f47128d48fd890e
baranowb <baranowb@gmail.com>
2019-02-27 12:05:43 +0100

    Fix patch operation

--- bisect-script.sh
+++ bisect-script.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 set -eo pipefail
 
-readonly GITHUB_REPO=${GITHUB_REPO:-'git@github.com:jbossas/jboss-eap7.git'}
-readonly GITHUB_BRANCH=${GITHUB_BRANCH:-'7.2.x-proposed'}
-readonly BISECT_WORKSPACE=${BISECT_WORKSPACE:-$(mktemp -d)}
+readonly GITHUB_REPO="${GITHUB_REPO:-'git@github.com:jbossas/jboss-eap7.git'}"
+readonly GITHUB_BRANCH="${GITHUB_BRANCH:-'7.2.x-proposed'}"
+readonly BISECT_WORKSPACE="${BISECT_WORKSPACE:-$(mktemp -d)}"
 
 #git clone "${GITHUB_REPO}"  --single-branch --branch "${GITHUB_BRANCH}" "${BISECT_WORKSPACE}"
 git clone "${GITHUB_REPO}"  --branch "${GITHUB_BRANCH}" "${BISECT_WORKSPACE}"
 cd "${BISECT_WORKSPACE}"
 
 #good revision, we consider current one as bad?
-readonly GOOD_REVISION=${GOOD_REVISION}
-readonly BAD_REVISION=${BAD_REVISION}
+readonly GOOD_REVISION="${GOOD_REVISION}"
+readonly BAD_REVISION="${BAD_REVISION}"
 #url of a patch file (a diff) containing the changes required to insert
 # the reproducer into EAP existing testsuite.
-readonly REPRODUCER_PATCH_URL=${REPRODUCER_PATCH_URL}
+readonly REPRODUCER_PATCH_URL="${REPRODUCER_PATCH_URL}"
 #test to run from suite, either existing one or one that comes from $TEST_DIFF
-readonly TEST_NAME=${TEST_NAME:-"*.*TestCase"}
+readonly TEST_NAME="${TEST_NAME:-"*.*TestCase"}"
 set -u
 
 readonly REPRODUCER_PATCH=${PATCH_HOME:-$(mktemp)}
 curl "${REPRODUCER_PATCH_URL}" -o "${REPRODUCER_PATCH}"
-if [ ! -e "${REPRODUCER_PATCH}" ]; then
+if [ -e "${REPRODUCER_PATCH}" ]; then
   export REPRODUCER_PATCH
 else
-	echo "No reproducer patch!"
+	echo "No reproducer patch"
+	#return 1
 fi
 
 git bisect 'start'
--- run-test.sh
+++ run-test.sh
@@ -5,16 +5,20 @@ readonly REPRODUCER_PATCH="${REPRODUCER_PATCH}"
 readonly TEST="${TEST_NAME}}"
 
 set -u
-echo "${TEST}"
-echo "${REPRODUCER_PATCH}"
-echo "patch -p1 -i ${REPRODUCER_PATCH}"
-patch -p1 -i "${REPRODUCER_PATCH}"
+if [ -e "${REPRODUCER_PATCH}" ]; then
+	echo "Patching ...."
+	patch -p1 -i "${REPRODUCER_PATCH}"
+else
+	echo "No patch file provided, skipping"
+fi
+
 # TODO if patch fails, we need to skip test and print a message that the test is not compatible with the revision skipped
 
 if [ -z "${TEST}" ]; then
   echo 'No TEST provided.'
-  echo 1
+  return 1
 fi
+
 
 echo -n 'Building ...'
 
@@ -27,5 +31,7 @@ echo -n 'Running testsuite ...'
 export TESTSUITE_OPTS="-Dtest=$TEST"
 /opt/jboss-set-ci-scripts/harmonia-eap-build 'testsuite'
 
-echo -n 'Cleaning up after patch ...'
-patch -p1 -i ${REPRODUCER_PATCH} -R
+if [ -e "${REPRODUCER_PATCH}" ]; then
+	echo -n 'Cleaning up after patch ...'
+patch -p1 -i "${REPRODUCER_PATCH}" -R
+fi
