5dae2e437d68f1a8f8ec60c02e0bf89c50ba41de
baranowb <baranowb@gmail.com>
2019-02-27 08:15:32 +0100

    Change vars to be passed from env not as args

--- bisect-script.sh
+++ bisect-script.sh
@@ -2,13 +2,13 @@
 set -eo pipefail
 
 #good revision, we consider current one as bad?
-readonly GOOD_REVISION=${1}
-readonly BAD_REVISION=${2}
+readonly GOOD_REVISION=${GOOD_REVISION}
+readonly BAD_REVISION=${BAD_REVISION}
 #url of a patch file (a diff) containing the changes required to insert
 # the reproducer into EAP existing testsuite.
-readonly REPRODUCER_PATCH_URL=${2}
+readonly REPRODUCER_PATCH_URL=${REPRODUCER_PATCH_URL}
 #test to run from suite, either existing one or one that comes from $TEST_DIFF
-readonly TEST_NAME=${TEST_NAME:-"*.Test" }
+readonly TEST_NAME=${TEST_NAME:-"*.*TestCase" }
 set -u
 
 readonly REPRODUCER_PATCH=${PATCH_HOME:-$(mktemp)}
