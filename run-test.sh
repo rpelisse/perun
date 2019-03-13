47b93676a588a59b041c983c32cfa7a92c4a183d
Romain Pelisse <belaran@gmail.com>
2019-03-13 16:57:39 +0100

    Increase scripts resiliency, some cleaning and add unit tests (bats)

--- run-test.sh
+++ run-test.sh
@@ -2,14 +2,36 @@
 set -eo pipefail
 
 cleanPatch() {
-if [ -e "${REPRODUCER_PATCH}" ]; then
-  echo '[PERUN]: Cleaning up after patch ...'
-  patch -p1 -i "${REPRODUCER_PATCH}" -R
-  patch -p1 -i "${INTEGRATION_SH_PATCH}" -R
-fi
+  if [ -e "${REPRODUCER_PATCH}" ]; then
+    echo '[PERUN]: Cleaning up after patch ...'
+    patch -p1 -i "${REPRODUCER_PATCH}" -R
+    patch -p1 -i "${INTEGRATION_SH_PATCH}" -R
+  fi
 }
 
 trap cleanPatch EXIT
+
+log() {
+  local mssg="${@}"
+  local perun_log_prefix=${PERUN_LOG_PREFIX:-'[PERUN]'}
+
+  echo "${perun_log_prefix} ${mssg}"
+}
+
+set +e
+which patch > /dev/null
+if [ "${?}" -ne 0 ]; then
+  log "This script requires 'patch' command, but command is missing. Aborting."
+fi
+set -e
+
+if [ ! -e "${HARMONIA_SCRIPT}" ]; then
+  log "Invalid path to Harmonia script provided: ${HARMONIA_SCRIPT}. Aborting."
+  exit 1
+fi
+
+readonly PERUN_LOG_PREFIX=${PERUN_LOG_PREFIX:-'[PERUN]'}
+readonly GIT_SKIP_BISECT_ERROR_CODE=${GIT_SKIP_BISECT_ERROR_CODE:-'125'}
 
 readonly REPRODUCER_PATCH="${REPRODUCER_PATCH}"
 readonly TEST="${TEST_NAME}"
@@ -17,44 +39,43 @@ readonly HARMONIA_SCRIPT="${HARMONIA_SCRIPT:-'/opt/jboss-set-ci-scripts/harmonia
 
 readonly CURRENT_REVISION=$(git rev-parse HEAD)
 if [[ $CORRUPT_REVISIONS == *"${CURRENT_REVISION}"* ]]; then
-  echo "[PERUN]: Current revision \"${CURRENT_REVISION}\" is in corrupt list, skipping."
-  #125 - special git value to indicate `bisect skip`
-  exit 125
+  log "Current revision \"${CURRENT_REVISION}\" is in corrupt list, skipping."
+  exit "${GIT_SKIP_BISECT_ERROR_CODE}"
 fi
+
 set -u
 
 if [ -e "${INTEGRATION_SH_PATCH}" ]; then
-  echo "[PERUN]: Patching integration script...."
+  log "Patching integration script...."
   patch -p1 -i "${INTEGRATION_SH_PATCH}"
 else
-  echo "[PERUN]: No integration.sh patch file provided, skipping"
+  log "No integration.sh patch file provided, skipping"
   exit 1
 fi
 
 if [ -e "${REPRODUCER_PATCH}" ]; then
-  echo "[PERUN]: Patching tests...."
+  log "Patching tests...."
   patch -p1 -i "${REPRODUCER_PATCH}"
 else
-  echo "[PERUN]: No tests patch file provided, skipping"
+  log "No tests patch file provided, skipping"
 fi
 
 # TODO if patch fails, we need to skip test and print a message that the test is not compatible with the revision skipped
 
 if [ -z "${TEST}" ]; then
-  echo '[PERUN]: No TEST provided.'
+  log "No TEST provided."
   exit 1
 fi
 
+log "Building ..."
 
-echo '[PERUN]: Building ...'
-
-export BUILD_OPTS="-DskipTests"
+export BUILD_OPTS=${BUILD_OPTS:"-DskipTests"}
 bash -x ${HARMONIA_SCRIPT}
 
-echo '[PERUN]: Done.'
+log "Done."
 
-echo '[PERUN]: Running testsuite ...'
-export TESTSUITE_OPTS="-Dtest=$TEST"
-echo "[PERUN]: Start testsuite"
+log "Running testsuite ..."
+export TESTSUITE_OPTS="${TESTSUITE_OPTS} -Dtest=${TEST}"
+log "Start testsuite"
 bash -x ${HARMONIA_SCRIPT} 'testsuite'
-echo "[PERUN]: Stop testsuite"
+log "Stop testsuite"
